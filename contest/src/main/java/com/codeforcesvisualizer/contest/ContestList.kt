8b31542b23ca5f07d08f2a53f1487c67662b0822
Md Didarul islam <islamdidarmd@gmail.com>
2022-03-01 23:50:22 +0600

    Update contest list UI

--- contest/src/main/java/com/codeforcesvisualizer/contest/ContestList.kt
+++ contest/src/main/java/com/codeforcesvisualizer/contest/ContestList.kt
@@ -1,23 +1,63 @@
 package com.codeforcesvisualizer.contest
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.Colors
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.codeforcesvisualizer.domain.entity.Contest
+import com.codeforcesvisualizer.domain.entity.Phase
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun ContestList(
     modifier: Modifier,
     contestList: List<Contest>
 ) {
+    val upcoming = contestList.filter { it.phase == Phase.BEFORE }
+    val past = contestList.filter { it.phase != Phase.BEFORE }
+    val grouped = linkedMapOf<String, List<Contest>>()
+
+    if (upcoming.isNotEmpty()) grouped["Upcoming"] = upcoming
+    grouped["Past"] = past
+
     LazyColumn(modifier = modifier) {
-        items(contestList) { contest ->
-            Card {
-                Text(text = contest.name)
+        grouped.forEach { (title, list) ->
+            stickyHeader {
+                Header(modifier = Modifier, text = title)
             }
+            items(list) { contest ->
+                ContestListItem(modifier = Modifier, contest = contest)
+            }
+        }
+    }
+}
+
+@Composable
+internal fun Header(modifier: Modifier, text: String) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.LightGray)
+    ) {
+        Text(text = text, modifier = Modifier.padding(8.dp))
+    }
+}
+
+@Composable
+internal fun ContestListItem(modifier: Modifier, contest: Contest) {
+    Card(modifier = modifier
+        .fillMaxWidth()
+        .padding(8.dp)) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            Text(text = contest.name)
         }
     }
 }
