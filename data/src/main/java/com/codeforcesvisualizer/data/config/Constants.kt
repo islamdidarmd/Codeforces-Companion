bd96d475f3f4c1b8ebb9293b88ba29b63eba777d
Md Didarul islam <islamdidarmd@gmail.com>
2022-03-03 19:56:42 +0600

    Removed clear text flag from Manifest

--- data/src/main/java/com/codeforcesvisualizer/data/config/Constants.kt
+++ data/src/main/java/com/codeforcesvisualizer/data/config/Constants.kt
@@ -1,7 +1,7 @@
 package com.codeforcesvisualizer.data.config
 
 const val BASE_URL = "https://codeforces.com"
-const val CONTEST_LIST_URL = "http://codeforces.com/api/contest.list"
+const val CONTEST_LIST_URL = "https://codeforces.com/api/contest.list"
 
 const val OKHTTP_READ_TIMEOUT = 30L
 const val OKHTTP_CONNECT_TIMEOUT = 30L
