1ca02b0e0111fdae3536906e9c7dface38581f18
Md Didarul islam <islamdidarmd@gmail.com>
2022-03-14 23:04:07 +0600

    Add Calender Button in Details Screen

--- core/src/main/java/com/codeforcesvisualizer/core/data/utils/TimeUtils.kt
+++ core/src/main/java/com/codeforcesvisualizer/core/data/utils/TimeUtils.kt
@@ -5,6 +5,18 @@ import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.*
 
+fun Int.convertToDHMS(): String {
+    val days = this / (3600 * 24)
+    val hours = (this % (3600 * 24)) / 3600
+    val minutes = (this % 3600) / 60
+    val seconds = this % 60
+    return buildString {
+        if(days > 0) append(String.format("%02d Days ", days))
+        if (hours > 0) append(String.format("%02d hr ", hours))
+        if (minutes > 0) append(String.format("%02d min ", minutes))
+        if (seconds > 0) append(String.format("%02d sec", seconds))
+    }
+}
 fun Int.convertToHMS(): String {
     val hours = this / 3600
     val minutes = (this % 3600) / 60
@@ -15,6 +27,20 @@ fun Int.convertToHMS(): String {
         if (seconds > 0) append(String.format("%02d sec", seconds))
     }
 }
+
+fun Long.convertToDHMS(): String {
+    val days = this / (3600 * 24)
+    val hours = (this % (3600 * 24)) / 3600
+    val minutes = (this % 3600) / 60
+    val seconds = this % 60
+    return buildString {
+        if(days > 0) append(String.format("%02d Days ", days))
+        if (hours > 0) append(String.format("%02d hr ", hours))
+        if (minutes > 0) append(String.format("%02d min ", minutes))
+        if (seconds > 0) append(String.format("%02d sec", seconds))
+    }
+}
+
 fun Long.convertToHMS(): String {
     val hours = this / 3600
     val minutes = (this % 3600) / 60
