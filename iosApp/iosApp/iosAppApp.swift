b1aa59473895935fc9ed09449d4f289ff651d8d6
Md Didarul islam <islamdidarmd@gmail.com>
2025-10-25 23:09:08 +0800

    Create ios app

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Md Didarul Islam on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
