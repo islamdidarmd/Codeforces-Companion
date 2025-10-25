75d71a38cd0bd0770d36bbdffc6b7a4af8fb7677
Md Didarul islam <islamdidarmd@gmail.com>
2025-10-25 23:09:08 +0800

    Create ios app

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Md Didarul Islam on 21/10/2025.
+//
+
+import SwiftUI
+import sharedKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
